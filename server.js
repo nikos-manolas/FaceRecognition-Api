19ab4cb82d54b31282546e3f043df865ac00961b
Nikos <nikos.manolas@gmail.com>
2018-11-27 16:38:13 +0000

    fixing database connection to heroku postgres

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '101423',
-    database : 'smartbrain'
+    host: PROCESS.env.DATABASE_URL,
+    ssl: true
   }
 });
 
